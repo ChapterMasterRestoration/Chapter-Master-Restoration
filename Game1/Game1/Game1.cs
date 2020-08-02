b5dce6be4b6195ad46654d504f50c19e7a83d0e0
obscuredcode <essamacmillan@gmail.com>
2020-08-03 01:30:23 +0300

    First syeps and primitive rednering plan

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -13,11 +13,16 @@ namespace Game1
         GraphicsDeviceManager graphics;
         GraphicsDevice graphicsDevice;
         SpriteBatch spriteBatch;
-        private Texture2D test;
+        public const string IMAGE_DIRECTORY = "textures";
+        int WIDTH = 1280;
+        int HEIGHT = 960;
+        private Texture2D background;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = WIDTH;
+            graphics.PreferredBackBufferHeight = HEIGHT;
             Content.RootDirectory = "Content";
         }
 
@@ -38,16 +43,28 @@ namespace Game1
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
+        /// 
+        // metathesis
+        /* FileStream file = new FileStream("Content/images/spr_honor_helm_3.png", FileMode.Open);
+        graphicsDevice = graphics.GraphicsDevice;
+        test = Texture2D.FromStream(graphicsDevice, file);
+        file.Close(); */
+        private Texture2D LoadPNG(string name)
+        {
+            Texture2D texture;
+            FileStream file = new FileStream(Content.RootDirectory + "/" + IMAGE_DIRECTORY + "/" + name + ".png", FileMode.Open);
+            graphicsDevice = graphics.GraphicsDevice;
+            texture = Texture2D.FromStream(graphicsDevice, file);
+            file.Close();
+            return texture;
+        }
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             // TODO: use this.Content to load your game content here
-            // metathesis
-            FileStream file = new FileStream("Content/images/spr_honor_helm_3.png", FileMode.Open);
-            graphicsDevice = graphics.GraphicsDevice;
-            test = Texture2D.FromStream(graphicsDevice, file);
-            file.Close();
+            background = LoadPNG("background/bg_space");
+
         }
 
         /// <summary>
@@ -84,8 +101,20 @@ namespace Game1
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(test,new Rectangle(0,0,167,80),Color.White);
+            // Move and Rotate Camera
+            float camX = 0; 
+            float camY = 0;
+            // Scale and Zoom
+            float scaleX = 1;
+            float scaleY = 1;
+                // Draw background
+            spriteBatch.Draw(background, new Rectangle(0, 0,1280,960), Color.White);
+            float zoom = 1;
+
+            // Draw systems
+            // Draw hyperlanes
             spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
