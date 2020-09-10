764e90307acc108fb4950674dc3b47ba78d8cd21
SireNuke <NUCLEARHZZARD22@gmail.com>
2020-09-11 01:09:08 +0300

    I, Cato Sicarius, have committed this code to Github because I, Cato Sicarius, have started work on factions, including the best faction of I, Cato Sicarius!

--- ChapterMaster/ChapterMaster/World/System.cs
+++ ChapterMaster/ChapterMaster/World/System.cs
@@ -8,13 +8,15 @@ namespace ChapterMaster.World
 {
     public class System
     {
-        public int id; // TODO: assign in constructor
+        public int id; // TO DO: assign in constructor
         public int color;
         public string name;
         public int x, y;
         public int numberOfLanes = 0;
         public List<Planet> Planets = new List<Planet>();
         public bool forgeProne;
+        public string DeJureFactionOwner;
+
         public System(int color, int x, int y)
         {
             this.color = color;
@@ -54,6 +56,19 @@ namespace ChapterMaster.World
             ChapterMaster.MainScreen.Screens.RemoveAll(predicate);
             view.PlanetScreenOpen = false;
             view.openSystem = -1;
+        }
+        public Dictionary<string, float> FindOwners()
+        {
+            Dictionary<string, float> factions = new Dictionary<string, float>();
+            for (int planetindex = 0; planetindex < Planets.Count; planetindex++)
+            {
+                if (!factions.ContainsKey(Planets[planetindex].FactionOwner))
+                {
+                    factions.Add(Planets[planetindex].FactionOwner, 0);
+                }
+                factions[Planets[planetindex].FactionOwner] = (float)1 / (float)Planets.Count;
+            }
+            return factions;
         }
     }
 }
