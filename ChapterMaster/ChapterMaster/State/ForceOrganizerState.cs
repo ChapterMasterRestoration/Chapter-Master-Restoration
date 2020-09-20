e7a01329ccd049d89a7856b19f56f58098e072c8
obscuredcode <essamacmillan@gmail.com>
2020-09-20 22:26:06 +0300

    Started work on Force Organizer.

---
+++ ChapterMaster/ChapterMaster/State/ForceOrganizerState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterMaster.State
+{
+    public class ForceOrganizerState : State
+    {
+        public ForceOrganizerState(GameManager gameManager, GraphicsDevice graphicsDevice, ContentManager contentManager) : base(gameManager, graphicsDevice, contentManager)
+        {
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Resize(GameWindow window)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChapterMaster/ChapterMaster/Tree/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterMaster.Tree
+{
+    // https://stackoverflow.com/a/2012855
+    delegate void TreeVisitor(Node node);
+    abstract class Node
+    {
+        private List<Node> children = new List<Node>();
+        public Node()
+        {
+
+        }
+
+        public void AddChild(Node node)
+        {
+            children.Add(node);
+        }
+
+        public void AddChildren(params Node[] node)
+        {
+            foreach (Node n in node)
+            {
+                children.Add(n);
+            }
+        }
+
+        public List<Node> GetChildren()
+        {
+            return children;
+        }
+        public void Traverse(Node node, TreeVisitor visitor)
+        {
+            visitor(node);
+            foreach (Node kid in node.children)
+                Traverse(kid, visitor);
+        }
+    }
+}
